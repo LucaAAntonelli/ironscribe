9eafb1462154d491003962f252ca8f29143a0291
Luca <luca.antonelli@gmx.ch>
2025-05-30 11:04:14 +0200

    started writing gRPC server (not working so far)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
+use tonic::{transport::Server, Request, Response, Status};
+
+use ironscribe_common::hello_world::greeter_server::{Greeter, GreeterServer};
+use ironscribe_common::hello_world::{HelloReply, HelloRequest};
+
+pub mod hello_world {
+    tonic::include_proto!("ironscribe-common/helloworld");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     println!("Hello, world from server!");
+
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder().add_service(GreeterServer::new(greeter)).serve(addr).await?;
+
+    Ok(())
 }
