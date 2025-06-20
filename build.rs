f64d8a193a672eb5f03085b2c5b5275cfe014c6e
Luca <luca.antonelli@gmx.ch>
2025-06-20 16:40:26 +0200

    fixed formatting

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = tonic_build::Config::new();
     config.type_attribute("booksync.Book", "#[derive(serde::Deserialize)]");
-    tonic_build::configure().build_client(true).build_server(true).compile_protos_with_config(config, &["proto/booksync.proto"], &["proto"]).expect("Failed to compile proto files!");
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .compile_protos_with_config(config, &["proto/booksync.proto"], &["proto"])
+        .expect("Failed to compile proto files!");
     Ok(())
 }
