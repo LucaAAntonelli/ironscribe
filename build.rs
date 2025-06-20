6b11e53920fdc69a9117fe0f0332f478bbdc1e6b
Luca <luca.antonelli@gmx.ch>
2025-06-20 16:39:57 +0200

    added build config to automatically derive serde::Deserialize on Book struct

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/booksync.proto")?;
+    let mut config = tonic_build::Config::new();
+    config.type_attribute("booksync.Book", "#[derive(serde::Deserialize)]");
+    tonic_build::configure().build_client(true).build_server(true).compile_protos_with_config(config, &["proto/booksync.proto"], &["proto"]).expect("Failed to compile proto files!");
     Ok(())
 }
