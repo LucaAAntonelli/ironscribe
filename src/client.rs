e14dd382902390d9cc440066155546578254ad4d
Luca <luca.antonelli@gmx.ch>
2025-07-02 16:15:31 +0200

    added initial module imports for generated code

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,9 @@
+use file_sync::dir_sync_client::DirSyncClient;
+
+pub mod file_sync {
+    tonic::include_proto!("service");
+}
+
 fn main() {
     println!("Hello world from client!");
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,10 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use file_sync::dir_sync_server::{DirSync, DirSyncServer};
+
+pub mod file_sync {
+    tonic::include_proto!("service");
+}
 fn main() {
     println!("Hello world from server!");
 }
