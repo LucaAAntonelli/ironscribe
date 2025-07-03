c0ab6d19691b930118cb28ca7fc8200a244f073d
Luca <luca.antonelli@gmx.ch>
2025-07-03 10:36:11 +0200

    added empty functions for DirSyncServer trait bounds to impl block

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,12 +1,12 @@
-use tonic::{Request, Response, Status};
-
-use shared::shared::{dir_sync_server::DirSync, HelloRequest, HelloResponse};
+use tonic::{Request, Response, Status, Streaming};
+use shared::shared::{dir_sync_server::DirSync, Block, ChecksumRequest, ChecksumResponse, DiffRequest, DiffResponse, HelloRequest, HelloResponse, SyncRequest, SyncResponse, UploadResponse};
 
 #[derive(Debug, Default)]
 pub struct MyDirSync {}
 
 #[tonic::async_trait]
 impl DirSync for MyDirSync {
+    
     async fn say_hello(
         &self,
         request: Request<HelloRequest>,
@@ -18,6 +18,34 @@ impl DirSync for MyDirSync {
         };
 
         Ok(Response::new(response))
+    }
+
+    async fn sync_structure(
+        &self,
+        request: Request<SyncRequest>,
+    ) -> Result<Response<SyncResponse>, Status> {
+        todo!("IMPLEMENT sync_structure()!");
+    }
+
+    async fn diff_structure(
+        &self,
+        request: Request<DiffRequest>,
+    ) -> Result<Response<DiffResponse>, Status> {
+        todo!("IMPLEMENT diff_structure()!");
+    }
+
+    async fn get_checksum(
+        &self,
+        request: Request<ChecksumRequest>,
+    ) -> Result<Response<ChecksumResponse>, Status> {
+        todo!("IMPLEMENT get_checksum()!");
+    }
+
+    async fn upload_blocks(
+        &self,
+        request: Request<Streaming<Block>>,
+    ) -> Result<Response<UploadResponse>, Status> {
+        todo!("IMPLEMENT upload_blocks()!");
     }
 }
 
