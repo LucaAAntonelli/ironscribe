a93e2b21723a58f30e1988645e09a016b98e3d5c
Luca <luca.antonelli@gmx.ch>
2025-07-04 23:53:16 +0200

    added clean_path() util

--- shared/src/filesystem.rs
+++ shared/src/filesystem.rs
@@ -1,6 +1,13 @@
-use std::{io::Error, fs::copy};
+use std::{fs::copy, io::Error, path::Path};
 
 pub fn force_copy(source: String, target: String) -> Result<(), Error> {
     copy(source, target)?;
     Ok(())
+}
+
+pub fn clean_path(path: String) -> String {
+    if path == "" {
+        return "".to_string();
+    }
+    Path::new(&path).canonicalize().unwrap().to_str().unwrap().to_owned()
 }
