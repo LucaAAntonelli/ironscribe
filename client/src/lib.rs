3ee1a48f5df22aa0f195ff1e7d45c8259a5c08ff
Luca <luca.antonelli@gmx.ch>
2025-07-26 11:32:12 +0200

    started writing client implementation

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,1 +1,16 @@
+use chrono::Utc;
 use shared::proto::dir_sync_client::DirSyncClient;
+use std::{error::Error, path::PathBuf};
+use tonic::transport::Channel;
+
+struct Client {
+    client: DirSyncClient<Channel>,
+}
+
+impl Client {
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            client: DirSyncClient::connect("http://[::1]:50051").await?,
+        })
+    }
+}
