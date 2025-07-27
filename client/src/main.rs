97e916718c4dfd99ec66a01d03c3332869079fb2
Luca <luca.antonelli@gmx.ch>
2025-07-27 13:09:23 +0200

    added new client implementation to client main

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,7 @@
-use shared::proto::dir_sync_client::DirSyncClient;
-use tonic::Request;
+use client::newfilesync::BookClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = BookClient::new("[::1]", 50051, None, None, None).await?;
     Ok(())
 }
