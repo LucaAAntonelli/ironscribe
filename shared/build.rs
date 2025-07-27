83fd78c52c0a765ea4be54f50f08a976ddefb73b
Luca <luca.antonelli@gmx.ch>
2025-07-27 10:34:08 +0200

    added new protobuf definition to start over with services

--- shared/build.rs
+++ shared/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/filesync.proto")?;
+    tonic_build::compile_protos("proto/newfilesync.proto")?;
     Ok(())
 }
