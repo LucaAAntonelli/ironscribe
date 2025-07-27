42181d3801e21e007e39012306e8836eb4ab2ef5
Luca <luca.antonelli@gmx.ch>
2025-07-27 14:25:12 +0200

    started implementing basic client App

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,23 @@
 use client::newfilesync::BookClient;
 use eframe::egui;
+use tonic::transport::Channel;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = BookClient::new("[::1]", 50051, None, None, None).await?;
     Ok(())
+}
+
+struct MyApp {
+    name: String,
+    client: BookClient<Channel>,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Hello World".to_string(),
+            client: BookClient::new("[::1]", 50051, None, None, None).await?;
+        }
+    }
 }
