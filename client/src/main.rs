f4c8d0ad3e4bd8a0f6e626b5990e6dbdbe19e70b
Luca <luca.antonelli@gmx.ch>
2025-07-27 21:43:42 +0200

    added egui hello world app example

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,23 +1,63 @@
 use client::newfilesync::BookClient;
-use eframe::egui;
+use eframe::{CreationContext, egui};
 use tonic::transport::Channel;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = BookClient::new("[::1]", 50051, None, None, None).await?;
-    Ok(())
+fn main() {
+    let mut options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "HELLOWORLD",
+        options,
+        Box::new(|cc| Ok(Box::<MyApp>::default())),
+    );
 }
 
 struct MyApp {
+    rt: tokio::runtime::Runtime,
     name: String,
-    client: BookClient<Channel>,
+    age: u32,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
+            rt: tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap(),
             name: "Hello World".to_string(),
-            client: BookClient::new("[::1]", 50051, None, None, None).await?;
+            age: 42,
         }
+    }
+}
+
+impl MyApp {
+    fn new(ctx: &CreationContext) -> Self {
+        ctx.egui_ctx.set_pixels_per_point(1.25);
+        Self {
+            rt: tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .unwrap(),
+            name: "Some name".to_string(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Helllo '{}', age {}", self.name, self.age));
+        });
     }
 }
