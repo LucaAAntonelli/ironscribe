c477d3fe698e5e43ee404824f24f84c8f2f10041
Luca <luca.antonelli@gmx.ch>
2025-07-28 06:59:41 +0200

    removed hello world app stuff from GUI

--- client/src/main.rs
+++ client/src/main.rs
@@ -16,8 +16,6 @@ fn main() {
 
 struct MyApp {
     rt: tokio::runtime::Runtime,
-    name: String,
-    age: u32,
     file_dialog: FileDialog,
     picked_file: Option<PathBuf>,
 }
@@ -29,8 +27,6 @@ impl Default for MyApp {
                 .enable_all()
                 .build()
                 .unwrap(),
-            name: "Hello World".to_string(),
-            age: 42,
             file_dialog: FileDialog::new(),
             picked_file: None,
         }
@@ -41,23 +37,14 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Helllo '{}', age {}", self.name, self.age));
             if ui.button("Select file").clicked() {
                 println!("Send gRPC add book request");
                 self.file_dialog.pick_file();
             }
             self.file_dialog.update(ctx);
             if let Some(path) = self.file_dialog.take_picked() {
-                println!("User selected: {:?}", path);
+                self.picked_file = Some(path);
+                println!("User selected: {:?}", self.picked_file);
             }
         });
     }
