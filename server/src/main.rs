dbe9818bde4ececd94a4ca437636bbe5966415d2
Luca <luca.antonelli@gmx.ch>
2025-07-29 21:12:10 +0200

    working on getting server and client to work

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,24 @@
+use anyhow::anyhow;
 use server::newfilesync::BookServer;
 use shared::proto::book_sync_server::BookSyncServer;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use tokio::net::TcpListener;
 use tonic::transport::Server;
+use tonic::transport::server::TcpIncoming;
+use tracing::debug;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let booksync = BookServer::default();
+    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 50051);
+    let listener = TcpListener::bind(socket_addr).await?;
+    let local_addr = listener.local_addr()?;
+    let listener = TcpIncoming::from_listener(listener, true, None).map_err(|e| anyhow!(e))?;
+    debug!("Created server at directory {}", booksync.get_directory());
 
     Server::builder()
         .add_service(BookSyncServer::new(booksync))
-        .serve(addr)
+        .serve(socket_addr)
+        .serve_with_incoming(listener)
         .await?;
 
     Ok(())
--- server/src/newfilesync.rs
+++ server/src/newfilesync.rs
@@ -1,35 +1,75 @@
-use anyhow::anyhow;
+use anyhow::{Result, anyhow};
 use shared::proto::{
     AddBookRequest, AddBookResponse, add_book_request, book_sync_server::BookSync,
 };
-use std::{path::PathBuf, sync::Arc};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
 use tokio_stream::StreamExt;
+use tonic::transport::{Certificate, Identity, ServerTlsConfig};
 use tonic::{Request, Response, Status, Streaming};
-use tracing::error;
+use tracing::{error, instrument};
 
-#[derive(Default)]
+fn create_tls_config(
+    cert_path: &Path,
+    key_path: &Path,
+    ca_cert_path: &Path,
+) -> Result<ServerTlsConfig> {
+    let cert = std::fs::read_to_string(cert_path)?;
+    let key = std::fs::read_to_string(key_path)?;
+    let ca_cert = std::fs::read_to_string(ca_cert_path)?;
+
+    let identity = Identity::from_pem(cert, key);
+    let client_ca_cert = Certificate::from_pem(ca_cert);
+    let tls_config = ServerTlsConfig::new()
+        .identity(identity)
+        .client_ca_root(client_ca_cert);
+
+    Ok(tls_config)
+}
+
+#[derive(Debug)]
 pub struct BookServer {
     directory: Arc<PathBuf>,
 }
 
+impl Default for BookServer {
+    fn default() -> Self {
+        Self {
+            directory: Arc::new(PathBuf::from(
+                "C:\\Users\\lucaa\\Projects\\ironscribe\\TESTING",
+            )),
+        }
+    }
+}
+
 impl BookServer {
+    #[instrument]
     pub fn new(directory: PathBuf) -> Self {
+        println!("Creating BookServer instance!");
         Self {
             directory: Arc::new(directory),
         }
+    }
+
+    pub fn get_directory(&self) -> String {
+        self.directory.to_str().to_owned().unwrap().to_string()
     }
 }
 // TODO: Add traces for more information about program execution
 #[tonic::async_trait]
 impl BookSync for BookServer {
+    #[instrument]
     async fn add_book(
         &self,
         request: Request<Streaming<AddBookRequest>>,
     ) -> tonic::Result<Response<AddBookResponse>, Status> {
         let mut request_stream = request.into_inner();
         let directory = Arc::clone(&self.directory);
+        println!("Received request for directory: {:?}", directory);
 
         let task_handle = tokio::spawn(async move {
             let file_name = if let Some(file_upload) = request_stream.next().await {
@@ -65,6 +105,7 @@ impl BookSync for BookServer {
             error!(%err);
             Err(Status::internal("Failed to upload file"))
         } else {
+            println!("Successfully added book!");
             Ok(Response::new(AddBookResponse::default()))
         }
     }
