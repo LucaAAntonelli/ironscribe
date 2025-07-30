2ed84d38a3f1ec69fc045546ebcb66dd3afb6c58
Luca <luca.antonelli@gmx.ch>
2025-07-30 19:59:11 +0200

    removed more unused stuff

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::anyhow;
 use server::newfilesync::BookServer;
 use shared::proto::book_sync_server::BookSyncServer;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
@@ -11,7 +10,6 @@ use tonic::transport::server::TcpIncoming;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 50051);
     let listener = TcpListener::bind(socket_addr).await?;
-    let local_addr = listener.local_addr()?;
     let listener = TcpIncoming::from(listener);
 
     let file_service_impl = BookServer::new(
