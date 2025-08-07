2055617b719e4cae175f54fa0174d85861ef9fcd
Luca <luca.antonelli@gmx.ch>
2025-08-07 07:12:39 +0200

    started adding eframe template stuff to allow GUI to run natively and in web

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,7 @@
 use client::app::MyApp;
+
+// Main function for native GUI
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let options = eframe::NativeOptions::default();
     eframe::run_native(
@@ -6,4 +9,49 @@ fn main() {
         options,
         Box::new(|_cc| Ok(Box::<MyApp>::default())),
     );
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindget_futures::spawn_local(async {
+        let document = web_sys
+            .window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::<MyApp>::default())),
+            )
+            .await;
+
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    })
 }
