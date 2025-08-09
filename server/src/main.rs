07925ac3a715e04dfa52b20205cdcdd3175e8c7f
Luca <luca.antonelli@gmx.ch>
2025-08-09 10:53:43 +0200

    reset everything to blank slate, moved protobuf out of shared lib crate

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,30 +1,3 @@
-use server::newfilesync::BookServer;
-use shared::proto::book_sync_server::BookSyncServer;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::{path::PathBuf, str::FromStr};
-use tokio::net::TcpListener;
-use tonic::transport::Server;
-use tonic::transport::server::TcpIncoming;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 50051);
-    let listener = TcpListener::bind(socket_addr).await?;
-    let listener = TcpIncoming::from(listener);
-
-    let file_service_impl = BookServer::new(
-        PathBuf::from_str(
-            //     "C:\\Users\\Luca.Antonelli\\Documents\\Scripts\\ironscribe\\TESTING"
-            "/home/luca/tmp",
-        )
-        .unwrap(),
-    );
-    let file_service_server = BookSyncServer::new(file_service_impl);
-
-    Server::builder()
-        .add_service(file_service_server)
-        .serve_with_incoming(listener)
-        .await?;
-
-    Ok(())
+fn main() {
+    println!("Hello world!");
 }
