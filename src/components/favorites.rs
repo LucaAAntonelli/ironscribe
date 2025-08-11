26b0854b4968435a11ef7d9078681194e4fe3349
Luca <luca.antonelli@gmx.ch>
2025-08-11 22:03:09 +0200

    worked through Dioxus tutorial, implemented HotDog app

---
+++ src/components/favorites.rs
@@ -0,0 +1,21 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn Favorites() -> Element {
+    let mut favorites = use_resource(crate::backend::list_dogs).suspend()?;
+
+    rsx! {
+        div { id: "favorites",
+            div { id: "favorites-container",
+                for (id, url) in favorites().unwrap() {
+                    // Render a div for each photo using the dog's ID as the list key
+                    div {
+                        key: id,
+                        class: "favorite-dog",
+                        img { src: "{url}" }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+mod backend;
+mod components;
+use components::Favorites;
+use components::NavBar;
 use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
+
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
+#[derive(Routable, Clone, PartialEq)]
+enum Route {
+    #[layout(NavBar)]
+    #[route("/")]
+    DogView,
+
+    #[route("/favorites")]
+    Favorites,
+}
 
 fn main() {
     dioxus::launch(App);
@@ -11,6 +30,51 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        "HotDog!"
+        document::Stylesheet { href: MAIN_CSS }
+
+        Router::<Route> {}
+
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
+        div {
+            id: "title",
+            h1 { "HotDog! 🌭" }
+        }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+    rsx! {
+        div {
+            id: "dogview",
+            img { src: img_src.cloned().unwrap_or_default()}
+        }
+        div {
+            id: "buttons",
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip"},
+            button {
+                id: "save",
+                onclick: move |_| async move {
+
+                    let current = img_src.cloned().unwrap();
+                    img_src.restart();
+                    _ = backend::save_dog(current).await;
+                },
+            "save!"}
+        }
     }
 }
