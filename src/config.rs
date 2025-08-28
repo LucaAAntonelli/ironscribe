e661e31891c61a3126bad7e9a71980fcb87e5276
Luca <luca.antonelli@gmx.ch>
2025-08-28 06:53:58 +0200

    init_config() now creates folder and config file if they don't exist yet

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,23 @@
 use directories::ProjectDirs;
+use std::fs::{create_dir_all, File};
+use std::path::Path;
 
 pub fn init_config() {
     // Create app's OS-specific config directories
     // Linux: /home/<user>/.config/ironscribe
     // Windows: C:\Users\<user>\Appdata\Roaming\ironscribe\config
     if let Some(proj_dirs) = ProjectDirs::from("", "", "ironscribe") {
-        println!("{proj_dirs:?}");
+        let config_directory_path = proj_dirs.config_dir();
+        create_dir_all(config_directory_path);
+        let config_file_path = config_directory_path.join("config.json");
+        let config_file_path = config_file_path.as_path();
+
+        // Create config file if it doesn't exist yet
+        if std::path::Path::exists(std::path::Path::new(config_file_path)) {
+            // If file existed before, it may contain values already
+        } else {
+            // File doesn't exist yet -> need to create and will not contain values yet
+            File::create(config_file_path);
+        }
     }
 }
