3dcaef5f2b2c23f6a0d04382783de640fc64f758
Luca <luca.antonelli@gmx.ch>
2025-08-29 22:33:14 +0200

    create Config instance from config.json file and return it to get path (and future other config stuff) into the app

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,15 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, Context};
 use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
 use std::fs::{create_dir_all, File};
+use std::path::PathBuf;
 
-pub fn init_config() -> anyhow::Result<bool> {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub data_dir: Option<PathBuf>,
+}
+
+pub fn init_config() -> anyhow::Result<Config> {
     // Create app's OS-specific config directories
     // Linux: /home/<user>/.config/ironscribe
     // Windows: C:\Users\<user>\Appdata\Roaming\ironscribe\config
@@ -15,11 +22,15 @@ pub fn init_config() -> anyhow::Result<bool> {
         // Create config file if it doesn't exist yet
         if std::path::Path::exists(std::path::Path::new(config_file_path)) {
             // If file existed before, it may contain values already
-            return Ok(true);
+            let content =
+                std::fs::read_to_string(config_file_path).context("failed to read config file!")?;
+            let config: Config = serde_json::from_str(&content)
+                .context("failed to parse file content to Config object!")?;
+            return Ok(config); // config may still not contain path here!
         } else {
             // File doesn't exist yet -> need to create and will not contain values yet
             File::create(config_file_path)?;
-            return Ok(false);
+            return Ok(Config { data_dir: None }); // file was just created -> guaranteed to be empty
         }
     }
     Err(anyhow!("Failed to generate config folder path!"))
