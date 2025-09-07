98bc668b1e31bfbaf941609678b411edddced8d3
Luca <luca.antonelli@gmx.ch>
2025-09-07 19:02:53 +0200

    change config stuff to be methods on Config struct, add tests

--- src/services/config.rs
+++ src/services/config.rs
@@ -3,6 +3,45 @@ use crate::shared::types::Config;
 use anyhow::{anyhow, Context};
 use directories::ProjectDirs;
 use std::fs::{create_dir_all, File};
+use std::path::{Path, PathBuf};
+
+impl Config {
+    pub fn file_path() -> anyhow::Result<PathBuf> {
+        let proj_dirs =
+            ProjectDirs::from("", "", "ironscribe").context("failed to determine config path")?;
+        let config_path = proj_dirs.config_dir().join("config.json");
+        Ok(config_path)
+    }
+
+    pub fn init_config() -> anyhow::Result<()> {
+        let config_path = Self::file_path()?;
+        create_dir_all(config_path.clone())?;
+        if Path::exists(config_path.as_path()) {
+            return Err(anyhow!("File already exists!"));
+        } else {
+            File::create(config_path)?;
+        }
+        Ok(())
+    }
+
+    pub fn read_config() -> anyhow::Result<Self> {
+        let config_path = Self::file_path()?;
+        let content =
+            std::fs::read_to_string(config_path).context("Failed to read config file!")?;
+        match serde_json::from_str(&content) {
+            Ok(config) => Ok(config),
+            Err(e) => Err(anyhow!("Failed to deserialize Config: {}", e)),
+        }
+    }
+
+    pub fn save_config(&self) -> anyhow::Result<()> {
+        let config_path = Self::file_path()?;
+        let content = serde_json::to_string(self).context("Failed to serialize Config!")?;
+        std::fs::write(config_path, content)
+            .context("Failed to write serialized config to file!")?;
+        Ok(())
+    }
+}
 
 pub fn init_config() -> anyhow::Result<Config> {
     // Create app's OS-specific config directories
@@ -48,4 +87,18 @@ pub fn persist_config(config: Config) -> anyhow::Result<()> {
         return Ok(());
     }
     Err(anyhow!("Failed to generate config folder path!"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_path() {
+        #[cfg(target_os = "windows")]
+        let expected_path =
+            PathBuf::from("C:\\Users\\lucaa\\Appdata\\Roaming\\ironscribe\\config\\config.json");
+        let path = Config::file_path().unwrap();
+        assert_eq!(expected_path, path);
+    }
 }
