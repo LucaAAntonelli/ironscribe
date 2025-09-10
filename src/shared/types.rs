57f3eb17af433dc89f2d3ffe59b25864fd6a75aa
Luca <luca.antonelli@gmx.ch>
2025-09-10 08:04:55 +0200

    add comment to further explain use of Config struct

--- src/shared/types.rs
+++ src/shared/types.rs
@@ -3,5 +3,6 @@ use std::path::PathBuf;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Config {
+    // Directory chosen by user to store the application's SQLite file (library.db)
     pub data_dir: Option<PathBuf>,
 }
