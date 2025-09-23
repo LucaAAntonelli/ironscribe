f7c708ae30399967bdbcea688ab344b90f378183
Luca <luca.antonelli@gmx.ch>
2025-09-23 20:54:48 +0200

    move complete Config struct into backend, not really shared

--- shared/src/types.rs
+++ shared/src/types.rs
@@ -1,8 +1,2 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Config {
-    // Directory chosen by user to store the application's SQLite file (library.db)
-    pub data_dir: Option<PathBuf>,
-}
