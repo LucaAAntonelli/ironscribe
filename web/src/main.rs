cc8982c9250a16026e431623f9675883619b348c
Luca <luca.antonelli@gmx.ch>
2025-09-25 22:24:13 +0200

    remove no longer needed stuff from main function, config init now handled properly in ui

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,12 +1,5 @@
-#[cfg(feature = "server")]
-use backend::config::init_config;
 use ui::app::App;
 
 fn main() {
-    #[cfg(feature = "server")]
-    if let Err(e) = init_config() {
-        eprintln!("fatal: {e}");
-        std::process::exit(1);
-    }
     dioxus::launch(App);
 }
