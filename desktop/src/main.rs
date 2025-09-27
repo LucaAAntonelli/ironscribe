0eaac81d14f6c3a38e005ee15567e15d9836e962
Luca <luca.antonelli@gmx.ch>
2025-09-27 21:24:49 +0200

    fix issues on desktop, adjust main function and dependency according to web

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,13 +1,5 @@
-use backend::config::init_config;
 use ui::app::App;
 
 fn main() {
-    #[cfg(feature = "server")]
-    {
-        if let Err(e) = init_config() {
-            eprintln!("fatal: {e}");
-            std::process::exit(1);
-        }
-    }
     dioxus::launch(App);
 }
